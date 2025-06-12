677fe0be0d68e50d3d3d6154f6359acb3a4b3ff7
upi1682 <upi538287@gmail.com>
2025-06-12 18:16:13 +0700

    membuat clas public

--- Circular.cpp
+++ Circular.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 using namespace std;
 
+//CircularQueueArray_4NIMBelakang
+
 class Queues {
     int FRONT, REAR, max = 5;
     int queue_array[5];
-}
+
+public:
+    Queues(){
+        FRONT =-1;
+        REAR = -1;
+    }
+
+   
+    
+
+
+  
+};
+
